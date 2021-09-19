2ee8153cc82f5545bfde2cbc90c1daa5872c40c1
a sd <starcraft0529@gmail.com>
2021-09-19 08:12:33 +0000

    [update] : Finance와 Plan component의 기초 내용 구성

--- abcd/src/components/Finance.js
+++ abcd/src/components/Finance.js
@@ -1,7 +1,7 @@
 import './Finance.scss';
 import NavBar from './NavBar.js';
-import { Tabs, Tab } from 'react-bootstrap';
-import { Bar, Line, Pie } from 'react-chartjs-2';
+import { Tabs, Tab, Table } from 'react-bootstrap';
+import { Pie } from 'react-chartjs-2';
 
 function Finance(){
 	const data = {
@@ -70,12 +70,108 @@ function Finance(){
 			</section>
 			<section className={'budget'}>
 				<div className={'section-title'}>이번달 예산 편성</div>
+				<Table striped bordered hover>
+				  <thead>
+					<tr>
+					  <th>통장 이름</th>
+					  <th>목적 및 설명</th>
+					  <th>책정 예산</th>
+					</tr>
+				  </thead>
+				  <tbody>
+					<tr>
+					  <td>적금통장</td>
+					  <td>데이트비용, 종잣돈을 위한 군적금을 모으는 통장</td>
+					  <td>200,000</td>
+					</tr>
+					<tr>
+					  <td>경제적독립일투자통장</td>
+					  <td>장기적 투자로 경제적 독립을 이루기 위한 통장</td>
+					  <td>200,000</td>
+					</tr>
+					<tr>
+					  <td>필수생활통장</td>
+					  <td>Larry필수생활비를 위한 통장(자동이체 등록)</td>
+					  <td>33,000</td>
+					</tr>
+					<tr>
+					  <td>품위유지 및 자기투자통장</td>
+					  <td>품위유지 및 자기투자를 위한 통장</td>
+					  <td>80,000</td>
+					</tr>
+					<tr>
+					  <td>개인지출통장</td>
+					  <td>기호에 따른 소비 통장</td>
+					  <td>20,000</td>
+					</tr>
+					<tr>
+					  <td>선물통장</td>
+					  <td>선물을 위한 통장</td>
+					  <td>30,000</td>
+					</tr>
+					<tr>
+					  <td>짜투리통장</td>
+					  <td>위 통장들에 나눠넣고 남은 돈을 넣는 통장</td>
+					  <td>-</td>
+					</tr>
+					<tr>
+					  <td>송금용더미통장</td>
+					  <td>송금을 위한 더미통장</td>
+					  <td>-</td>
+					</tr>
+				  </tbody>
+				</Table>
 			</section>
 			<section className={'investment-status'}>
 				<div className={'section-title'}>투자상황</div>
+				<div>포트폴리오, 자산 비율, 수익률 등 이것도 table로</div>
+				<Table striped bordered hover>
+				  <thead>
+					<tr>
+					  <th>tick</th>
+					  <th>비율</th>
+					  <th>유형</th>
+					</tr>
+				  </thead>
+				  <tbody>
+					<tr>
+					  <td>QQQ</td>
+					  <td>25%</td>
+					  <td>주식</td>
+					</tr>
+					<tr>
+					  <td>TQQQ</td>
+					  <td>75%</td>
+					  <td>주식</td>
+					</tr>
+				  </tbody>
+				</Table>
+				<div>수익률 : 25%</div>
 			</section>
 			<section className={'account-book'}>
 				<div className={'section-title'}>가계부</div>
+				<div>지출내역</div>
+				<Table striped bordered hover>
+				  <thead>
+					<tr>
+					  <th>설명</th>
+					  <th>가격</th>
+					  <th>통장</th>
+					</tr>
+				  </thead>
+				  <tbody>
+					<tr>
+					  <td>px이용</td>
+					  <td>5,000</td>
+					  <td>개인지출통장</td>
+					</tr>
+					<tr>
+					  <td>일기장구매</td>
+					  <td>2,870</td>
+					  <td>품위유지 및 자기투자통장</td>
+					</tr>
+				  </tbody>
+				</Table>
 			</section>
 		</main>
 
--- abcd/src/components/Plan.js
+++ abcd/src/components/Plan.js
@@ -1,6 +1,7 @@
 import './Plan.scss';
 import NavBar from './NavBar.js';
-
+import planImg from '../images/plan.jpg';
+import { Table } from 'react-bootstrap';
 
 function Plan(){
   return (
@@ -14,12 +15,24 @@ function Plan(){
 		<main>
 			<section className={'week-plan'}>
 				<div className={'section-title'}>이번주 계획표</div>
+				<div>img파일? 아니면 자체적으로 레이아웃?</div>
+				<img src={planImg} alt={'planimg'}></img>
 			</section>
 			<section className={'habits'}>
 				<div className={'section-title'}>습관</div>
+				<div>habit 리스트</div>
+				<ul>
+					<li>물 3통 마시기</li>
+					<li>하루 2회 세안</li>
+					<li>이틀에 한 번은 가족, 여자친구에게 전화 돌리기</li>
+					<li>일기 쓰기</li>
+					<li>독후감 쓰기</li>
+					<li>책 읽기</li>
+				</ul>
 			</section>
 			<section className={'todo'}>
 				<div className={'section-title'}>할 일</div>
+				<div>체크리스트 재사용해서 완료하면 사라지게</div>
 			</section>
 		</main>
 
